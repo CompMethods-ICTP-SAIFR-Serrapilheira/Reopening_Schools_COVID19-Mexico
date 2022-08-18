6af3efa3d3940f3b9d5090e93d1b9220086b2624
RuthCoronaMoreno <ruth.corona.m@gmail.com>
2022-08-17 21:45:17 -0500

    Update of storage path of functions

--- codes/R/run_simulation.R
+++ codes/R/run_simulation.R
@@ -1,35 +1,50 @@
-library(dplyr) # >%>
+#Purpose:Simulate data of COVID-19 epidemic dynamic in a university in Mexico
+#Author: Ruth Corona-Moreno
+
+library(dplyr) 
 library(interp)
-library(tidyr) #expand function
+library(tidyr) 
 library(plotly)
 source("codes/R/fun_simulations.R", local = TRUE)
 
-phi <- 0.3
-Sp <- 0.97
-Se <- 0.80
+
+#*************************************************************************
+#------------------------Set parameters values----------------------------
+
+#Location name and total population
+lugar.name <- 'QUERETARO_CAP'
+pop_tot <- 1.05*(10^6)
+
+#School population initial conditions
+vac <- 'YES'
+esc.size <- 1000
 TP0inic <-  0 
 FP0inic <- 0
-estimations_date <- '2021-09-26'
-TimeWindow <- 3
-pop_tot <- 1.05*(10^6)
-tau.ext <- 0
-vac <- 'YES'
-lugar.name <- 'QUERETARO_CAP'
-esc.size <- 1000
 D0inic <- 0
 S0inic <- 0 
 Rec0inic <- 8
-b.esc <- 0.1
-w <- 0
+ci <- 3 #E0inic=A0inic=ci
+
+#Estimations from covidestim
+estimations_date <- '2021-09-26'
+TimeWindow <- 3
+
+#Vaccination conditions
 type.test <- 3
 tau <- 0.05
-ci <- 3
+tau.ext <- 0
+Sp <- 0.97
+Se <- 0.80
+
+#Parameters of the model
+b.esc <- 0.1
+w <- 0
+phi <- 0.3
+TauMax=esc.size/2 #Required to define the linear screening strategy
 
 
-#########
-#tau_Func
-TauMax=esc.size/2
-
+#*************************************************************************
+#------------------------Time windows definition---------------------------
 if(TimeWindow==1){
   inic_esc='2020-08-02'
   fin_esc= '2020-12-17'
@@ -44,6 +59,8 @@ if(TimeWindow==1){
 }
 
 
+#*************************************************************************
+#--------------------------Making simulations-----------------------------
 x <- modelo_func(
   vacuna=vac,
   b.esc=b.esc,
@@ -56,9 +73,10 @@ x <- modelo_func(
   E0inic=ci 
 )
 
+#Saving simulations
 df_simulations <- data.frame(x$fechas_simul,x$x.simul.Simul.U, x$simul.E, x$simul.A, x$simul.TP, x$simul.FP, x$simul.S, x$simul.R, x$simul.D, x$simul.beta.v, x$simul.rt_esc, x$simul.tests)
 write.csv(df_simulations, paste("output/",lugar.name,"_",inic_esc,"_",fin_esc,"_Vac",vac,"_bEsc",b.esc,"_Tot",esc.size,"_w",w,"_Test",type.test,"_tau",tau,"_A0E0",ci,".csv", sep=""))
 
 
-############   Plot   ##############################
+#Plot simulations
 plot_func(lugar.name, inic_esc, fin_esc, vac, b.esc, esc.size, w, type.test, tau, ci)
